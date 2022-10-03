b337403bf5ded915fb544c4fee6a96be8ebd8b78
Joshua Moore <Jmoore0177@conestogac.on.ca>
2022-10-03 19:20:28 -0400

    added regex for checking character is valid that player entered

--- TicTacToe/tttFunctions.cpp
+++ TicTacToe/tttFunctions.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
-#include <ctype.h>
+#include <regex>
 using namespace std;
 void intro()
 {
+    cout << "Welcome to TicTacToe!" << endl;
+    cout << "Players will take turns entering which square to mark, as shown below" << endl << endl;
     char board[9] = { '1','2','3','4','5','6','7','8','9' };
     cout << " " << board[0] << " " << "|" << " " << board[1] << " " << "|" << " " << board[2] << " " << endl;
     cout << "-----------" << endl;
     cout << " " << board[3] << " " << "|" << " " << board[4] << " " << "|" << " " << board[5] << " " << endl;
     cout << "-----------" << endl;
-    cout << " " << board[6] << " " << "|" << " " << board[7] << " " << "|" << " " << board[8] << " " << endl;
+    cout << " " << board[6] << " " << "|" << " " << board[7] << " " << "|" << " " << board[8] << " " << endl << endl;
 }
 
 char getGuess()
@@ -18,8 +20,10 @@ char getGuess()
     string input = "";
     while (true)
     {
-        getline(cin, input);
-        if (input.length() == 1 && isdigit(input[0]))
+        cin >> input;
+        const string regexInput = input;
+        regex r("[1-9]");
+        if (input.length() == 1 && regex_match(regexInput,r))
         {
             square = input[0];
             return square;
